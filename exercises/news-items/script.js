c90131c2508c092f61c9e9528a320ebfd7f06dd0
Sina Jazayeri <sina@colorcode.io>
2019-10-06 14:20:13 -0400

    added title to my HTML

--- exercises/news-items/script.js
+++ exercises/news-items/script.js
@@ -1,36 +1,63 @@
 var ul = document.getElementsByTagName('ul')[0];
+var data;
 
-for (var i = 0; i < newsItems.length; i++) {
-  var item = newsItems[i];
+getData();
 
-  // Create list item
-  var li = document.createElement('li');
-
-  addThisToTheList('h4', item.date, li);
-  addThisToTheList('h6', item.author, li);
-  addThisToTheList('p', item.summary, li);
-  addThisToTheList('img', item.image, li);
-
-  // Add list item to list
-  ul.appendChild(li);
-};
-
-function addThisToTheList (type, text, container) {
-  // Create the element
-  var element = document.createElement(type);
-
-  // Add some text to it
-  if (type === 'img') {
-    element.src = text;
-  } else {
-    element.innerText = text;
-  }
-
-  // Add child to container
-  container.appendChild(element);
+async function getData() {
+  var url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=52a47a6baa854ff484d64f4c1cfaa38a';
+  var request = new Request(url);
+  var response = await fetch(request);
+  var responseBody = await response.json();
+  data = responseBody.articles;
+  addItemsToPage();
 }
 
+function addItemsToPage() {
+  for (var i = 0; i < data.length; i++) {
+    var item = data[i];
+    
+    // Create list item
+    var li = document.createElement('li');
+    li.className = 'news-item';
+    li.id = 'item' + i;
+    
+    // Create Network h5
+    var network = document.createElement("div");
+    network.className = "news-item__network";
+    network.innerText = item.author;
+    li.appendChild(network);
 
+    // Create Div for Details (h5)
+    var detailsDiv = document.createElement('div');
+    detailsDiv.className = 'news-item__details';
+    li.appendChild(detailsDiv);
+
+    // Add image to div detail
+    var img = document.createElement('img');
+    img.src = item.urlToImage;
+    img.className = 'news-item__image';
+    detailsDiv.appendChild(img);
+
+    // Create Div for text
+    var textDiv = document.createElement('div');
+    textDiv.className = "news-item__text";
+    detailsDiv.appendChild(textDiv);
+
+    // Create Date container (p)
+    var date = document.createElement("p");
+    date.innerText = item.publishedAt;
+    date.className = "news-item__date";
+    textDiv.append(date);
+
+    // Create Title container (p)
+    var title = document.createElement("p");
+    title.innerText = item.title;
+    textDiv.append(title);
+    
+    // Add list item to list
+    ul.appendChild(li);
+  };
+}
 
 
 
